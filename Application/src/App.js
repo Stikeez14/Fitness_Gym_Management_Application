2230078b7a0135560564e5495cb557aaba2b09de
Beniamin-Emanuel Imbrescu <160716189+Stikeez14@users.noreply.github.com>
2024-04-02 12:15:55 +0300

    First Commit -> Specification, Use Case Diagram & Sign In page

---
+++ Application/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignUpForm from './SignUpForm'; // Assuming SignUpForm.js is in the same directory
+
+const App = () => {
+  return (
+    <div>
+      <h1>Fitness Gym Management Application</h1>
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default App;
